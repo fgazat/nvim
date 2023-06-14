aab0261cec86b2423e8bfd9a4ef3a29ae1cfde06
Azat Khasanov <aza8048@yandex.ru>
2023-06-14 20:58:28 +0200

    Init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# neovim
+# Nvim
+
+Todo: 
+
+* folding;
---
+++ init.lua
@@ -0,0 +1,14 @@
+require "user.lazy"
+require "user.keybindings"
+require('impatient')
+require "user.cmp"
+require "user.options"
+require "user.treesitter"
+require "user.lsp"
+require "user.lualine"
+require "user.dap"
+require "user.harpoon"
+require "user.nvim-tree"
+-- require "user.lspsaga"
+-- require "user.ufo"
+require("symbols-outline").setup()
