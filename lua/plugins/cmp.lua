93c445082e078904226f998abd196d9c686d971f
Azat Khasanov <aza8048@yandex.ru>
2024-04-19 01:50:49 +0200

    Fix null ls and fix

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,9 +1,13 @@
 return {
-    { "hrsh7th/nvim-cmp", },
-    { "hrsh7th/cmp-buffer", },  -- buffer completions,
-    { "hrsh7th/cmp-path", },    -- path completions,
-    { "hrsh7th/cmp-cmdline", }, -- cmdline completions,
-    { "hrsh7th/cmp-nvim-lsp", },
-    { "hrsh7th/cmp-emoji", },
-    { "hrsh7th/cmp-nvim-lua", },
+
+
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-emoji",
+        "hrsh7th/cmp-nvim-lua",
+    }
 }
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,9 +1,9 @@
 return {
 
     "nvimtools/none-ls.nvim",
-    lazy = true,
+    event = "BufEnter",
     opts = function()
-        null_ls = require("null-ls")
+        local null_ls = require("null-ls")
         local formatting = null_ls.builtins.formatting
         local diagnostics = null_ls.builtins.diagnostics
         return {
@@ -14,7 +14,6 @@ return {
                     extra_filetypes = { "toml", "solidity", "yaml", "json" },
                     extra_args = { "--single-quote", "--jsx-single-quote", "--print-width=120", "--prose-wrap=always" },
                 },
-                formatting.eslint,
                 formatting.black.with { extra_args = { "--line-length=120" } },
                 formatting.isort,
 
@@ -22,9 +21,7 @@ return {
                 formatting.gofumpt,
                 -- formatting.goimports,
 
-                formatting.shfmt,
                 -- formatting.go
-                diagnostics.shellcheck, -- shell script diagnostics
                 -- code_actions.shellchecklines,
                 -- code_actions.gomodifytags,
                 -- formatting.gofmt,
