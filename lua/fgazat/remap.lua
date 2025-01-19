e9cd7d8ccf3cdd7126b2c1f4d04df789e341db17
Azat Khasanov <aza8048@yandex.ru>
2025-01-19 22:06:13 +0100

    up

--- lua/fgazat/remap.lua
+++ lua/fgazat/remap.lua
@@ -9,7 +9,6 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("", "<space>", "<nop>")
-vim.keymap.set("", "<tab>", "<nop>")
 
 
 -- vim.keymap.set('i', '<c-h>', "<nop>")
