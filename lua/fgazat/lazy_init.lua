4bcb91d63e8880f4930fc2cf5941e3b847996402
fgazat <fgazat@yandex.ru>
2025-02-25 14:28:54 +0100

    up

--- lua/fgazat/lazy_init.lua
+++ lua/fgazat/lazy_init.lua
@@ -18,14 +18,10 @@ require("lazy").setup({
     spec = "fgazat.lazy",
     change_detection = { notify = false },
     defaults = {
-        -- Set this to `true` to have all your plugins lazy-loaded by default.
-        -- Only do this if you know what you are doing, as it can lead to unexpected behavior.
         lazy = true,
     },
     install = {
-        -- install missing plugins on startup. This doesn't increase startup time.
         missing = true,
-        -- try to load one of these colorschemes when starting an installation during startup
-        -- colorscheme = { "default" },
+        colorscheme = { "default" },
     },
 })
